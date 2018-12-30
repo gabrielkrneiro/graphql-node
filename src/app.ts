a92d382ece5bafd070130cc1cde4e740ba3df88b
Gabriel Carneiro <gabrielcarneirocef@gmail.com>
2018-12-30 13:32:36 -0400

    feat: dataloaders added in application context

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,22 @@ import * as graphqlHTTP from 'express-graphql';
 import db from './models'
 import schema from './graphql/schema';
 import { extractJwtMiddleware } from './middlewares/extract-jwt.middleware';
+import { DataLoaderFactory } from './dataloaders/DataLoaderFactory';
 
 class App {
 
     public express: Application;
+    public dataLoaderFactory: DataLoaderFactory;
 
     constructor() {
 
         this.express = express();
+        this.init();
+    }
+
+    private init(): void {
+
+        this.dataLoaderFactory = new DataLoaderFactory(db);
         this.middleware();
     }
 
@@ -25,7 +33,8 @@ class App {
         this.express.use('/graphql', 
             extractJwtMiddleware(),
             (req, res, next) => { 
-                req['context'].db = db;
+                req['context']['db'] = db;
+                req['context']['dataloaders'] = this.dataLoaderFactory.getLoaders(); // guarda os dataloaders no contexto
                 next();
             }, 
             graphqlHTTP(req => ({
--- src/interfaces/ResolverContextInterface.ts
+++ src/interfaces/ResolverContextInterface.ts
@@ -1,9 +1,14 @@
 import { DbConnection } from "./DBConnectionInterface";
 import { AuthUser } from "./AuthUserInterface";
+import { DataLoaders } from "./DataLoadersInterface";
 
+/**
+ *  tell what attributes application context may has
+ */
 export interface ResolverContext {
 
     db?: DbConnection;
     authorization?: string;
     authUser?: AuthUser;
+    dataLoaders?: DataLoaders;
 }
